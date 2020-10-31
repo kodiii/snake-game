dc6369a34bf73d5a5875a4841df6988f975d553c
Ricardo <codzign@gmail.com>
2020-10-31 18:35:44 +0100

    control function

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const grid = document.querySelector('.grid')
 const startBtn = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
+let currentSnake = [2, 1, 0]
+let direction = 1
+let width = 10
+
 
 function createGrid() {
     //create elements
     //create 100 of these elements
     for (let i = 0; i < 100; i++) {
-        // console.log(i)
         const square = document.createElement('div')
         //add styling to the element
         square.classList.add('square')
@@ -15,7 +18,46 @@ function createGrid() {
         grid.append(square)
         //push squares into squares array
         squares.push(square)
-        // console.log(squares)
     }
 }
 createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+function move() {
+    //remove last element from our currentSnake array
+    const tail = currentSnake.pop()
+    //remove styling from last element
+    squares[tail].classList.remove('snake')
+    //add sqaure in direction we are heading
+    currentSnake.unshift(currentSnake[0] + direction)
+    //add styling so we can see it
+    squares[currentSnake[0]].classList.add('snake')
+}
+move()
+
+let timerId = setInterval(move, 500)
+clearTimeout(timerId)
+
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+    if (e.which === 39) {
+        console.log('right pressed:')
+        direction = +1
+    } else if (e.which === 38) {
+        console.log('up pressed:')
+        direction = -width
+    } else if (e.which === 37) {
+        console.log('left pressed:')
+        direction = -1
+    } else if (e.which === 40) {
+        console.log('down pressed:')
+        direction = +width
+    }
+}
+
+document.addEventListener('keydown', control)
