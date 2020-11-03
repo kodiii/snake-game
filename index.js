a847b4aa7f60a9b6b601cc6a38d906b70ef3419e
Ricardo <codzign@gmail.com>
2020-11-03 20:39:51 +0100

    increase snake body and add apple

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const grid = document.querySelector('.grid')
 const startBtn = document.getElementById('start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
-let width = 10
+const width = 10
+let appleIndex = 0
+let score = 0
 
 
 function createGrid() {
     //create elements
     //create 100 of these elements
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < width * width; i++) {
         const square = document.createElement('div')
         //add styling to the element
         square.classList.add('square')
@@ -25,6 +27,15 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+        return clearInterval(timerId)
+
     //remove last element from our currentSnake array
     const tail = currentSnake.pop()
     //remove styling from last element
@@ -33,11 +44,39 @@ function move() {
     currentSnake.unshift(currentSnake[0] + direction)
     //add styling so we can see it
     squares[currentSnake[0]].classList.add('snake')
+
+    //deal with snake head getting the apple
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        //remove the class of apple
+        squares[currentSnake[0]].classList.remove('apple')
+        squares[appleIndex].textContent = ''
+        //grow our snake by adding class of snake to it
+        squares[tail].classList.add('snake')
+        //grow our snake array
+        currentSnake.push(tail)
+        //generate a new apple
+        generateApple()
+        //add one to the score
+        score++
+        //display our score
+        scoreDisplay.textContent = score
+        //speed up our snake
+
+    }
 }
 move()
 
 let timerId = setInterval(move, 500)
-clearTimeout(timerId)
+
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake')) {
+        squares[appleIndex].classList.add('apple')
+        squares[appleIndex].textContent = '🍎'
+    }
+}
+generateApple()
 
 // 39 is right arrow
 // 38 is for the up arrow
